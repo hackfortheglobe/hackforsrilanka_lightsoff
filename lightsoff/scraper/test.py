101b48e7b27fb78fa415d42bf27f97a18327808d
Rafael Acosta <rafaco@gmail.com>
2022-05-05 14:45:51 -0700

    Revert "Scraper: Real scraper implementation instead of hardcoded one"
    This reverts commit 36f1a48b2fdc3d42ba3061c042a908838ad50767.

--- lightsoff/scraper/test.py
+++ lightsoff/scraper/test.py
@@ -1,12 +1,11 @@
 
-import os
 from scraper import scrape
 
 if __name__ == "__main__":
     
-    #print("Starting test, passing current doc id, should be skipped...")
-    #scrape("1DN_Oxe3X6q1W02wgNzMT88HXX-vk3xFJ")
+    print("Starting test, passing current doc id, should be skipped...")
+    scrape("1DN_Oxe3X6q1W02wgNzMT88HXX-vk3xFJ")
 
     print()
-    print("Passing empty doc id, should be always processed...")
+    print("Now passing empty doc id, should be processed...")
     scrape("")
