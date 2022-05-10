584af83d881a2085b4977f72f4a78845df5764bf
Balram Y <balram.wangoes@gmail.com>
2022-05-10 22:09:03 +0530

    added script file

---
+++ scripts/periodic_task_renew.py
@@ -0,0 +1,9 @@
+from django_celery_beat.models import PeriodicTask, PeriodicTasks
+
+def run():
+	obj = PeriodicTask.objects.filter(name="scrapper_data").first()
+	if obj:
+		obj.last_run_at = None
+		obj.save()
+		PeriodicTasks.changed(obj)
+		print("############Call")
