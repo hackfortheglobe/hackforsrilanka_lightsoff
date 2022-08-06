65db3943a0c245d9974ae7936c32dbb92e522680
bansal-neeraj <sales@yorker.in>
2022-08-06 17:22:56 +0530

    sms test function

--- lightsoff/tasks.py
+++ lightsoff/tasks.py
@@ -1,6 +1,9 @@
 import datetime
 import socket
 from itertools import groupby
+
+import pytz
+
 from conf.celery import app
 from django.core.mail import send_mail
 from datetime import timedelta
@@ -322,13 +325,98 @@ def message_generator(group_schedule,group_name):
         msg_text = ''
         for i in obj_list:
             if i.starting_period != i.ending_period:
+                # print(i.starting_period.astimezone(tz=local_time))
                 from_date = format(i.starting_period,'M dS')
                 # from_date = i.starting_period.strftime('%b %-dth')
-                from_time = i.starting_period.strftime('%I:%M %p')
-                to_time =i.ending_period.strftime('%I:%M %p')
+                from_time = i.starting_period.astimezone(tz=local_time).strftime('%I:%M %p')
+                to_time = i.ending_period.astimezone(tz=local_time).strftime('%I:%M %p')
+
+                # from_time = i.starting_period.strftime('%I:%M %p')
+                # to_time =i.ending_period.strftime('%I:%M %p')
                 msg_text += f"{from_date} from {from_time} to {to_time}, "
         msg_text += f"[Group {group_name} power cut schedule].To unsubscribe go to {link}"
         received = yield msg_text
         # received = yield group_schedule[group_name]
         group_name = received if received is not None else None
 
+
+# ------------------------code for testing--------------------------
+
+@app.task(bind=True, max_retries=0)
+def test_send_sms_notification(self):
+    link = f"https://ekata.lk/unsubscribe"
+    url = "https://e-sms.dialog.lk/api/v1/sms"
+    headers = CaseInsensitiveDict()
+    access_token = login_sms_api()
+    headers["Authorization"] = f"Bearer {access_token}"
+    headers["Content-Type"] = "application/json"
+    d = datetime.datetime(2022,8,3,0,0,0,tzinfo=pytz.UTC)
+    schedule_group = ScheduleGroup.objects.filter(starting_period__gte=d).select_related().order_by('group_name')
+
+    if len(schedule_group) != 0:
+        schedule_dict = {key: list(gr) for key, gr in (groupby(schedule_group, key=lambda x: x.group_name.name))}
+        first_group = list(schedule_dict.keys())[0]
+        msg_gen_obj = message_generator(schedule_dict, first_group)
+        msg_gen_obj.send(None)
+        group_set = set()
+
+    for schedule_data in schedule_group:
+        if schedule_data.group_name.name != 'A' and schedule_data.group_name.name != 'M':
+            continue
+
+        all_sub = Subscriber.objects.filter(group_name=schedule_data.group_name,
+                                            is_unsubscribed=False)
+        if len(all_sub) == 0:
+            print(f"there is not subscriber for this group name {schedule_data.group_name.name}")
+            continue
+        sub_user = all_sub.values(mobile=F("mobile_number"))
+        schedule_data.is_run = True
+        schedule_data.save()
+        if schedule_data.group_name.name not in group_set:
+            group_set.add(schedule_data.group_name.name)
+            message = msg_gen_obj.send(schedule_data.group_name.name)
+        else:
+            continue
+
+        paginator = Paginator(sub_user, 100)
+        for page_no in paginator.page_range:
+            current_page = paginator.get_page(page_no)
+            current_qs = current_page.object_list
+            tx_id = generate_uniqe_id()
+            # numbers = list(current_qs)
+            numbers = ['123456789']
+            resp = send_sms(numbers, message, tx_id)
+            if resp.status_code == 200:
+                res_data = resp.json()
+                tx_data = Transaction.objects.create(campaingn_id=res_data["data"].get("campaignId", None),
+                                                     campaingn_cost=res_data["data"].get("campaignCost", None),
+                                                     user_id=res_data["data"].get("userId", None),
+                                                     status="SUCCESS",
+                                                     tx_id=tx_id)
+                batch_data = Batch.objects.create(transaction=tx_data,
+                                                  status="SUCCESS",
+                                                  is_batch_run=True,
+                                                  message=message,
+                                                  schedule=schedule_data)
+                batch_data.subscriber.set(list(current_qs.values_list("id", flat=True)))
+                batch_data.save()
+                print("Batch has been run successfully.")
+            else:
+                res_data = resp.json()
+                tx_data = Transaction.objects.create(status="FAILED",
+                                                     tx_id=tx_id)
+                batch_data = Batch.objects.create(transaction=tx_data,
+                                    status="FAILED",
+                                    message=message,
+                                    schedule=schedule_data)
+                batch_data.subscriber.set(list(current_qs.values_list("id", flat=True)))
+                batch_data.save()
+                time = datetime.datetime.now(tz=local_time) + timedelta(minutes=5)
+                clock_time = ClockedSchedule.objects.create(clocked_time=time)
+                PeriodicTask.objects.create(clocked=clock_time,
+                                            one_off=True,
+                                            task="lightsoff.tasks.send_sms_to_batch",
+                                            name=f'send_batch_sms_{batch_data.id}')
+                print(f"Task is created for this batch number {batch_data.id}.")
+            # schedule_data.is_run = True
+            # schedule_data.save()
